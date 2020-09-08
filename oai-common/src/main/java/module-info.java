3211c995bfc78ddd0ff1c88b1cabffaa6115820f
Jörg Prante <joergprante@gmail.com>
2020-09-08 15:59:53 +0200

    update to content 2.4.0, netty 4.1.51

--- oai-common/src/main/java/module-info.java
+++ oai-common/src/main/java/module-info.java
@@ -4,6 +4,7 @@ module org.xbib.oai {
     exports org.xbib.oai.rdf;
     exports org.xbib.oai.util;
     exports org.xbib.oai.xml;
+    requires org.xbib.content.core;
     requires org.xbib.content.rdf;
     requires org.xbib.content.resource;
     requires org.xbib.content.xml;
